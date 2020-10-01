1bb41e2092d6b5e8d11b6ceade78057f112045ab
code5am <70967850+code5am@users.noreply.github.com>
2020-10-01 18:43:44 +0530

    Create code5am.java

---
+++ 5.Java/code5am.java
@@ -0,0 +1,10 @@
+class code5am {
+    public static void main(String[] args) {
+
+    String name = "Sreelakshmi";
+    int age = *20* ;
+    String username = "code5am";
+
+        System.out.println("Hello!\nName: "+name+"\nAge: "+age+ "\nUsername: "+username);
+    }
+}
