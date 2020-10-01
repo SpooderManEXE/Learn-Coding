3810e7717de2e337638f3c2e8272c35503d0708a
rahul-rana22 <72229692+rahul-rana22@users.noreply.github.com>
2020-10-02 02:25:30 +0530

    Create rahul-rana22.java

---
+++ 5.Java/rahul-rana22.java
@@ -0,0 +1,10 @@
+class rahul-rana22 {
+    public static void main(String[] args) {
+
+    String name = "Rahul Rana";
+    int age = 20 ;
+    String username = "rahul-rana22";
+
+        System.out.println("Hello!\nName: "+name+"\nAge: "+age+ "\nUsername: "+username);
+    }
+}
