6faae0a060db391ea9669a911f05f895815edae2
sree2511 <72211844+sree2511@users.noreply.github.com>
2020-10-01 20:11:56 +0530

    Create sree2511.java

---
+++ 5.Java/sree2511.java
@@ -0,0 +1,10 @@
+class sree2511 {
+    public static void main(String[] args) {
+
+    String name = "Sree";
+    int age = 20 ;
+    String username = "sree2511";
+
+        System.out.println("Hello!\nName: "+name+"\nAge: "+age+ "\nUsername: "+username);
+    }
+}
