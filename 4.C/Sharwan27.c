94b218c9609748f6e755167e2d3bfa6778609c03
Sharwan27 <62832912+Sharwan27@users.noreply.github.com>
2020-10-01 22:39:35 +0530

    Create Sharwan27.c

---
+++ 4.C/Sharwan27.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+ int main()
+  {
+     printf("Hello!\n");
+     printf("Name   : *Sharwan27\n");
+     printf("Age    : 16\n");
+     printf("Username : Sharwan27\n");
+     return(0);
+  }
