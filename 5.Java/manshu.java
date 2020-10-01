0a9e85d0e06c607cb4e6223699f4d969c40813bd
manshusr <72151648+manshusr@users.noreply.github.com>
2020-10-01 21:43:31 +0530

    Create manshu.java

---
+++ 5.Java/manshu.java
@@ -0,0 +1,10 @@
+class manshusr {
+    public static void main(String[] args) {
+
+    String name = "manshu";
+    int age = 21 ;
+    String username = "manshusr";
+
+        System.out.println("Hello!\nName: "+name+"\nAge: "+age+ "\nUsername: "+username);
+    }
+}
