8dd5b2f2d2e0d374fb43856ad4eb30850a43c117
hari94codes <72208337+hari94codes@users.noreply.github.com>
2020-10-01 19:22:17 +0530

    Create hari94codes.c

---
+++ 4.C/hari94codes.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+ int main()
+  {
+     printf("Hello!\n");
+     printf("Name   : Harikrishnan\n");
+     printf("Age    : 25\n");
+     printf("Username : hari94codes\n");
+     return(0);
+  }
