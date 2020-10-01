17e0abc05a4aa12c73e85ecd06df0d72f896064b
code5am <70967850+code5am@users.noreply.github.com>
2020-10-01 18:41:13 +0530

    Create code5am.c

---
+++ 4.C/code5am.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+ int main()
+  {
+     printf("Hello!\n");
+     printf("Name   : Sreelakshmi\n");
+     printf("Age    : 20\n");
+     printf("Username : code5am\n");
+     return(0);
+  }
