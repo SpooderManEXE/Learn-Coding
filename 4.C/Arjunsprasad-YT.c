06f7efd5e7a0f4068f4615a0cabad0523a3d9279
Arjunsprasad <72243394+Arjunsprasad@users.noreply.github.com>
2020-10-02 11:08:46 +0530

    Create Arjunsprasad-YT.c

---
+++ 4.C/Arjunsprasad-YT.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+ int main()
+  {
+     printf("Hello!\n");
+     printf("Name   : Arjun S Prasad\n");
+     printf("Age    : 19\n");
+     printf("Username : Arjunsprasad-YT\n");
+     return(0);
+  }
