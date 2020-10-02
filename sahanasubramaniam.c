4b73e1c74d94a4915cb0f08dcac5f4e1ae8c5b4e
sahanasubramaniam <72241940+sahanasubramaniam@users.noreply.github.com>
2020-10-02 10:20:10 +0530

    Create sahanasubramaniam.c

---
+++ sahanasubramaniam.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+ int main()
+  {
+     printf("Hello!\n");
+     printf("Name   : Sahana\n");
+     printf("Age    : 17\n");
+     printf("Username : sahanasubramaniam\n");
+     return(0);
+  }
