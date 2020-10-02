26b406bd98a517a369d25217ad36eea61e5f4373
aswanth2611 <72244919+aswanth2611@users.noreply.github.com>
2020-10-02 11:47:47 +0530

    Create aswanth2611.c

---
+++ 4.C/aswanth2611.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+ int main()
+  {
+     printf("Hello!\n");
+     printf("Name   : aswanth\n");
+     printf("Age    : 18\n");
+     printf("Username : aswanth2611\n");
+     return(0);
+  }
