d19af8dfc801e457813c4079de38fb086dea0678
harijith2611 <56642450+harijith2611@users.noreply.github.com>
2020-10-02 10:16:06 +0530

    Create harijith2611.c

---
+++ 4.C/harijith2611.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+ int main()
+  {
+     printf("Hello!\n");
+     printf("Name   : harijith\n");
+     printf("Age    : 20\n");
+     printf("Username : harijith2611\n");
+     return 0;
+  }
