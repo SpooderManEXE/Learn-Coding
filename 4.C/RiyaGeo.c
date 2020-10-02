ef7c9b489b9f1cf561ff08f07dd81666c2c1e64e
RiyaGeo <68366307+RiyaGeo@users.noreply.github.com>
2020-10-02 18:42:50 +0530

    Create RiyaGeo.c

---
+++ 4.C/RiyaGeo.c
@@ -0,0 +1,9 @@
+#include <stdio.h> 
+ int main()
+  {
+     printf("Hello!\n");
+     printf("Name   : RIYA\n");
+     printf("Age    : 22\n");
+     printf("Username : RiyaGeo\n");
+     return(0);
+  }
