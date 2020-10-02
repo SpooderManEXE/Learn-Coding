eab28a3e18c03a39139f4ec2590536f941cc3d49
jishnu2512 <56735160+jishnu2512@users.noreply.github.com>
2020-10-02 10:50:13 +0530

    Create jishnu2512.c

---
+++ 4.C/jishnu2512.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+ int main()
+  {
+     printf("Hello!\n");
+     printf("Name   : jishnu\n");
+     printf("Age    : 18\n");
+     printf("Username : jishnu2512\n");
+     return(0);
+  }
