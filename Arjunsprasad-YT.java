aa51406dc74f9d910d7633dd80997f8a12c092d6
Arjunsprasad <72243394+Arjunsprasad@users.noreply.github.com>
2020-10-02 11:11:25 +0530

    Create Arjunsprasad-YT.java

---
+++ Arjunsprasad-YT.java
@@ -0,0 +1,10 @@
+class *YOUR USERNAME* {
+    public static void main(String[] args) {
+
+    String name = "Arjun S Prasad";
+    int age = 19 ;
+    String username = "Arjunsprasad-YT";
+
+        System.out.println("Hello!\nName: "+name+"\nAge: "+age+ "\nUsername: "+username);
+    }
+}
