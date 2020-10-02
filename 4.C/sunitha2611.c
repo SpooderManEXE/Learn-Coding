a9d79de86b1aee4ff3ba68aeeb6113493e4e6271
sunitha2611 <72244064+sunitha2611@users.noreply.github.com>
2020-10-02 11:13:07 +0530

    Create sunitha2611.c

---
+++ 4.C/sunitha2611.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+ int main()
+  {
+     printf("Hello!\n");
+     printf("Name   : sunitha\n");
+     printf("Age    : 22\n");
+     printf("Username :sunitha2611\n");
+     return(0);
+  }
