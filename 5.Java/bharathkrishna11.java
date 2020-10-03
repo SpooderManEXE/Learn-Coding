97e3e91c718a249a8dcb4e8bee733931d5de23ca
bharathkrishna11 <53076517+bharathkrishna11@users.noreply.github.com>
2020-10-03 21:54:42 +0530

    Create bharathkrishna11.java

---
+++ 5.Java/bharathkrishna11.java
@@ -0,0 +1,10 @@
+class Bharath krishna {
+    public static void main(String[] args) {
+
+    String name = "Bharath krishna";
+    int age = 20 ;
+    String username = "bharathkrishna11";
+
+        System.out.println("Hello!\nName: "+name+"\nAge: "+age+ "\nUsername: "+username);
+    }
+}
