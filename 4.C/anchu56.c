78754042c2295b16f45ba37fbf3c5cbc22eda7d3
anchu56 <65988549+anchu56@users.noreply.github.com>
2020-10-03 09:00:56 +0530

    Create anchu56.c

---
+++ 4.C/anchu56.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+ int main()
+  {
+     printf("Hello!\n");
+     printf("Name   : Ansaf Muhammed\n");
+     printf("Age    : 19\n");
+     printf("Username : anchu56\n");
+     return(0);
+  }
