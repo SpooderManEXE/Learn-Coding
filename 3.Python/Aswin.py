686a84aea9cadea7c89aa63e391e027d1842c50c
AswinAzikar <65263396+AswinAzikar@users.noreply.github.com>
2020-10-03 15:04:16 +0530

    Update and rename #TEMPLATE.py to Aswin.py

--- 3.Python/Aswin.py
+++ 3.Python/Aswin.py
@@ -1,4 +1,4 @@
-name, age = "*YOUR NAME*", *YOUR AGE*
-username = "*YOUR USERNAME*"
+name, age = "Aswin MAnmathan S", *19*
+username = "*AswinAzikar*"
 print ('Hello!')
 print("Name: {}\nAge: {}\nUsername: {}".format(name, age, username))
