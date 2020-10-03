e0349fee8ebb52cb3bd8187b0424c7649b933ae4
AswinAzikar <65263396+AswinAzikar@users.noreply.github.com>
2020-10-03 16:24:57 +0530

    Create aswin.java

---
+++ 5.Java/aswin.java
@@ -0,0 +1,11 @@
+class AswinAzikar {
+    public static void main(String[] args) {
+
+    String name = "Aswin Manmathan s";
+    int age = 19 ;
+    String username = "AswinAzikar";
+
+        System.out.println("Hello!\nName: "+name+"\nAge: "+age+ "\nUsername: "+username);
+    }
+}
+© 2020 GitHub, Inc.
