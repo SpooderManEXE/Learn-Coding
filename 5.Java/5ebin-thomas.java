fed3fbb1eba06b277b8c0aedbb6f6797c291b57a
Sebin Thomas <56429311+5ebin-thomas@users.noreply.github.com>
2020-10-03 16:27:29 +0530

    Create 5ebin-thomas.java

---
+++ 5.Java/5ebin-thomas.java
@@ -0,0 +1,10 @@
+class 5ebin-thomas {
+    public static void main(String[] args) {
+
+    String name = "Sebin Thomas";
+    int age = 20 ;
+    String username = "5ebin-thomas";
+
+        System.out.println("Hello!\nName: "+name+"\nAge: "+age+ "\nUsername: "+username);
+    }
+}
