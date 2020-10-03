c19a34f9c611f7d3b89c28ae3aed6806f939dbda
bharathkrishna11 <53076517+bharathkrishna11@users.noreply.github.com>
2020-10-03 21:51:44 +0530

    Create bharathkrishna11.c

---
+++ 4.C/bharathkrishna11.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+ int main()
+  {
+     printf("Hello!\n");
+     printf("Name   : bharath krishna \n");
+     printf("Age    : 20\n");
+     printf("Username : bharathkrishna11 \n");
+     return(0);
+  }
