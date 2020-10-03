83ce59b36403fd6e27b0564cd4e6216b5025daa9
anchu56 <65988549+anchu56@users.noreply.github.com>
2020-10-03 09:14:51 +0530

    Create anchu56.java

---
+++ 5.Java/anchu56.java
@@ -0,0 +1,10 @@
+class anchu56 {
+    public static void main(String[] args) {
+
+    String name = "Ansaf Muhammed";
+    int age = 19 ;
+    String username = "anchu56";
+
+        System.out.println("Hello!\nName: "+name+"\nAge: "+age+ "\nUsername: "+username);
+    }
+}
