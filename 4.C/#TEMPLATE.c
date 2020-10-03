ab1c4223ad31bcbc0869e8049eba8b194eea11ee
AswinAzikar <65263396+AswinAzikar@users.noreply.github.com>
2020-10-03 15:06:11 +0530

    Update and rename 4.C/#TEMPLATE.c to Aswin.c

--- 4.C/#TEMPLATE.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h> 
- int main()
-  {
-     printf("Hello!\n");
-     printf("Name   : *YOUR NAME*\n");
-     printf("Age    : *YOUR AGE*\n");
-     printf("Username : *YOUR USERNAME*\n");
-     return(0);
-  }
---
+++ Aswin.c
@@ -0,0 +1,9 @@
+#include <stdio.h> 
+ int main()
+  {
+     printf("Hello!\n");
+     printf("Name   : Aswin\n");
+     printf("Age    : 19\n");
+     printf("Username : AswinAzikar\n");
+     return(0);
+  }
