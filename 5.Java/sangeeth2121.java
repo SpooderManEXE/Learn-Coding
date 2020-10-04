05a6990bbde4e1043228eb92e9f1fc543161e0fa
sangee2121 <72337654+sangee2121@users.noreply.github.com>
2020-10-04 17:50:54 +0530

    Create sangeeth2121.java

---
+++ 5.Java/sangeeth2121.java
@@ -0,0 +1,10 @@
+class sangee2121 {
+    public static void main(String[] args) {
+
+    String name = "Sangeeth";
+    int age = 20 ;
+    String username = "sangee2121";
+
+        System.out.println("Hello!\nName: "Sangeeth"\nAge: "20 "\nUsername: "sangee2121);
+    }
+}
