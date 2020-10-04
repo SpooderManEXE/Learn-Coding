ea11fdc05e5989e2335bba03600402e006a9d892
Sumeeha-Ashraf <72332068+Sumeeha-Ashraf@users.noreply.github.com>
2020-10-04 12:57:20 +0530

    Create Sumeeha-Ashraf.c

---
+++ 4.C/Sumeeha-Ashraf.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+ int main()
+  {
+     printf("Hello!\n");
+     printf("Name   : Sumeeha\n");
+     printf("Age    : 20\n");
+     printf("Username : Sumeeha-Ashraf\n");
+     return(0);
+  }
