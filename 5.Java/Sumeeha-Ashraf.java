420cb3fb6d358866b751adde6cbacac3867c12ea
Sumeeha-Ashraf <72332068+Sumeeha-Ashraf@users.noreply.github.com>
2020-10-04 13:01:00 +0530

    Create Sumeeha-Ashraf.java

---
+++ 5.Java/Sumeeha-Ashraf.java
@@ -0,0 +1,10 @@
+class Sumeeh-Ashraf {
+    public static void main(String[] args) {
+
+    String name = "Sumeeha";
+    int age = 20 ;
+    String username = "Sumeeha-Ashraf";
+
+        System.out.println("Hello!\nName: "+name+"\nAge: "+age+ "\nUsername: "+username);
+    }
+}
