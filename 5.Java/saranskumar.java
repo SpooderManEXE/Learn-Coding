fb4989e98ae206a35a134e4aeb33469e245568a2
Saran S Kumar <72315122+saranskumar@users.noreply.github.com>
2020-10-05 18:29:30 +0530

    Create saranskumar.java

---
+++ 5.Java/saranskumar.java
@@ -0,0 +1,10 @@
+class saranskumar {
+    public static void main(String[] args) {
+
+    String name = "Saran S Kumar";
+    int age = 13 ;
+    String username = "saranskumar";
+
+        System.out.println("Hello!\nName: "+name+"\nAge: "+age+ "\nUsername: "+username);
+    }
+}
