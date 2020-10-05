01a7122bfae8330ac203330dd46853f8605d3253
nithish9 <72372885+nithish9@users.noreply.github.com>
2020-10-05 16:47:58 +0530

    Create nithish9.c

---
+++ 4.C/nithish9.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+ int main()
+  {
+     printf("Hello!\n");
+     printf("Name   : NITHISH\n");
+     printf("Age    : 18\n");
+     printf("Username : nithish9\n");
+     return(0);
+  }
