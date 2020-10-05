9161570c0f98c7adeedb046f49d2c4cdede3e77c
Jacob Ranjit <52130837+Jcupzz@users.noreply.github.com>
2020-10-05 16:31:10 +0530

    Create Jcupzz.java

---
+++ 5.Java/Jcupzz.java
@@ -0,0 +1,10 @@
+class Jcupzz {
+    public static void main(String[] args) {
+
+    String name = "Jacob Ranjit";
+    int age = 19 ;
+    String username = "Jcupzz";
+
+        System.out.println("Hello!\nName: "+name+"\nAge: "+age+ "\nUsername: "+username);
+    }
+}
