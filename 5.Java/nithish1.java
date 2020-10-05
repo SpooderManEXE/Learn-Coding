43fbcb98e8be4db9733a4af1f322b390a98dccbb
nithish9 <72372885+nithish9@users.noreply.github.com>
2020-10-05 17:15:46 +0530

    Create nithish1.java

---
+++ 5.Java/nithish1.java
@@ -0,0 +1,10 @@
+class nithish9 {
+    public static void main(String[] args) {
+
+    String name = "NITHISH";
+    int age = 18 ;
+    String username = "nithish9";
+
+        System.out.println("Hello!\nName: "+name+"\nAge: "+age+ "\nUsername: "+username);
+    }
+}
