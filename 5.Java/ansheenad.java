d171f5580220249c6454609001a5109fd5dc24da
ansheena <67410523+ansheenad@users.noreply.github.com>
2020-10-06 16:18:05 +0530

    Create ansheenad.java

---
+++ 5.Java/ansheenad.java
@@ -0,0 +1,10 @@
+class ansheena {
+    public static void main(String[] args) {
+
+    String name = "ansheena";
+    int age = 19 ;
+    String username = "ansheenad";
+
+        System.out.println("Hello!\nName: "+name+"\nAge: "+age+ "\nUsername: "+username);
+    }
+}
