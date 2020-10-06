c25888c3c591c4e5b14acf63d7e4a161491989ab
jithinnk007 <56425438+jithinnk007@users.noreply.github.com>
2020-10-06 12:27:12 +0530

    Create jithinnk007.java

---
+++ 5.Java/jithinnk007.java
@@ -0,0 +1,10 @@
+class jithinnk007{
+    public static void main(String[] args) {
+
+    String name = "jithinnk";
+    int age = 23 ;
+    String username = "jithinnk007";
+
+        System.out.println("Hello!\nName: "+name+"\nAge: "+age+ "\nUsername: "+username);
+    }
+}
