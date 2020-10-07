cb4c19f521b2b64194ea7d7bb0f4fe5e20459d6a
SandeshSKrishnan <69599992+SandeshSKrishnan@users.noreply.github.com>
2020-10-07 19:16:46 +0530

    Create SandeshSKrishnan.java

---
+++ 5.Java/SandeshSKrishnan.java
@@ -0,0 +1,10 @@
+class SandeshSKrishnan {
+    public static void main(String[] args) {
+
+    String name = "Sandesh";
+    int age = 18 ;
+    String username = "SandeshSKrishnan";
+
+        System.out.println("Hello!\nName: "+name+"\nAge: "+age+ "\nUsername: "+username);
+    }
+}
