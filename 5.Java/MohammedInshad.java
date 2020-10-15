b9fbd8ee8a2ccb9ace318a789e5480bbc6e0be96
MohammedInshad <72918689+MohammedInshad@users.noreply.github.com>
2020-10-15 16:38:38 +0530

    Create MohammedInshad.java

---
+++ 5.Java/MohammedInshad.java
@@ -0,0 +1,10 @@
+class MohammedInshad {
+    public static void main(String[] args) {
+
+    String name = "Inshad";
+    int age = 18 ;
+    String username = "MohammedInshad";
+
+        System.out.println("Hello!\nName: "+name+"\nAge: "+age+ "\nUsername: "+username);
+    }
+}
