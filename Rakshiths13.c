bc73c44c79abb8b84f23bc715aa4ae768de1bbbc
Rakshith-s-13 <72958218+Rakshith-s-13@users.noreply.github.com>
2020-10-16 11:29:36 +0530

    Create Rakshiths13.c

---
+++ Rakshiths13.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+ int main()
+  {
+     printf("Hello!\n");
+     printf("Name   : *Rakshith*\n");
+     printf("Age    : *17*\n");
+     printf("Username : *Rakshiths13*\n");
+     return(0);
+  }
