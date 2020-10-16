4093c327915848d37104076971376d54c5fde103
Radhasubramaniam <72962812+Radhasubramaniam@users.noreply.github.com>
2020-10-16 11:50:12 +0530

    Create radhasubramaniam.c

---
+++ radhasubramaniam.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+ int main()
+  {
+     printf("Hello!\n");
+     printf("Name   : Radha\n");
+     printf("Age    : 51\n");
+     printf("Username : radhasubramaniam\n");
+     return(0);
+  }
