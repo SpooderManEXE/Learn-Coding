011c692ed2a3cd5b8bca57735f8dbca5581c1c0b
Remya101 <73032747+Remya101@users.noreply.github.com>
2020-10-18 09:55:00 +0530

    Create Remya101.c

---
+++ Remya101.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+ int main()
+  {
+     printf("Hello!\n");
+     printf("Name   : Remya IS\n");
+     printf("Age    : 36\n");
+     printf("Username : Remya101\n");
+     return(0);
+  }
