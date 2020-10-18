99390425c8672b77dd4103a1a5431ff8871ef24e
Remya101 <73032747+Remya101@users.noreply.github.com>
2020-10-18 10:01:57 +0530

    Create Remya101.java

---
+++ 5.Java/Remya101.java
@@ -0,0 +1,10 @@
+class Remya101 {
+    public static void main(String[] args) {
+
+    String name = "Remya I S";
+    int age = 36 ;
+    String username = "Remya101";
+
+        System.out.println("Hello!\nName: "+name+"\nAge: "+age+ "\nUsername: "+username);
+    }
+}
