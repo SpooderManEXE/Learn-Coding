02a624fd05cdfacd758e814361ab221ca3eda3b5
AaronGeorge341 <73268603+AaronGeorge341@users.noreply.github.com>
2020-10-22 10:34:16 -0400

    Create AaronGeorge341.py

---
+++ AaronGeorge341.py
@@ -0,0 +1,4 @@
+name, age = "Aaron George", 8
+username = "AaronGeorge341"
+print ('Welcome!')
+print("Name: {Aaron George}\nAge: {8}\nUsername: {AaronGeorge341}".format(Aaron George, 8, AaronGeorge341))
