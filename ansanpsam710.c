a4ab7b936b585b7388acc0dda4e837205a7ecbbb
Ansan P Sam <43809270+ansanpsam710@users.noreply.github.com>
2020-10-23 22:23:23 +0530

    Create ansanpsam710.c

---
+++ ansanpsam710.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+ int main()
+  {
+     printf("Hello!\n");
+     printf("Name   : ansanpsam\n");
+     printf("Age    : 18\n");
+     printf("Username : ansanpsam710\n");
+     return(0);
+  }
