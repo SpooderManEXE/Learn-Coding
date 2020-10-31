cf37136eeba0bc28dc30b99e19a2803026085585
iceee124 <73751786+iceee124@users.noreply.github.com>
2020-10-31 23:03:59 +0530

    Create iceee124.c

---
+++ 4.C/iceee124.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+ int main()
+  {
+     printf("Hello!\n");
+     printf("Name   : Aiswarya\n");
+     printf("Age    : 16\n");
+     printf("Username : iceee124\n");
+     return(0);
+  }
