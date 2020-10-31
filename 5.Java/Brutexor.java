72e5ad2bbc19e76e76b725392c4e936aff4e67df
Arjun K S <56349056+arjunks2112@users.noreply.github.com>
2020-10-31 17:26:02 +0530

    Create Brutexor.java

---
+++ 5.Java/Brutexor.java
@@ -0,0 +1,10 @@
+class Brutexor {
+    public static void main(String[] args) {
+
+    String name = "Arjun";
+    int age = 19 ;
+    String username = "arjunks2112";
+
+        System.out.println("Hello!\nName: "+name+"\nAge: "+age+ "\nUsername: "+username);
+    }
+}
