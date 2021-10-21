2407056dc9cddc79b6e344f56f3d66d25a1ea53c
AbdulAzees10 <92902856+AbdulAzees10@users.noreply.github.com>
2021-10-21 10:23:13 +0530

    Create AbdulAzees10.java

---
+++ 5.Java/AbdulAzees10.java
@@ -0,0 +1,10 @@
+class *YOUR USERNAME* {
+    public static void main(String[] args) {
+
+    String name = "Abdul Azees";
+    int age = 18 ;
+    String username = "AbdulAzees10";
+
+        System.out.println("Hello!\nName: "+name+"\nAge: "+age+ "\nUsername: "+username);
+    }
+}
