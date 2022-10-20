3191a8bd1084657daac7cde07dcd0bc40531e680
Justin V Kalappura <93908687+justinvkalappura@users.noreply.github.com>
2022-10-20 21:37:15 +0530

    Create justinvkalappura.java

---
+++ 5.Java/justinvkalappura.java
@@ -0,0 +1,10 @@
+class justinvkalappura {
+    public static void main(String[] args) {
+
+    String name = "justinvkalappura";
+    int age = 24 ;
+    String username = "justinvkalappura";
+
+        System.out.println("Hello!\nName: "+name+"\nAge: "+age+ "\nUsername: "+username);
+    }
+}
