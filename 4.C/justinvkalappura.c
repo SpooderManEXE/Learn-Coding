2e73506b3b64f2437dac97b1b431ffec779ae324
Justin V Kalappura <93908687+justinvkalappura@users.noreply.github.com>
2022-10-20 21:36:02 +0530

    Create justinvkalappura.c

---
+++ 4.C/justinvkalappura.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+ int main()
+  {
+     printf("Hello!\n");
+     printf("Name   : justinvkalappura\n");
+     printf("Age    : 24\n");
+     printf("Username : justinvkalappura\n");
+     return(0);
+  }
