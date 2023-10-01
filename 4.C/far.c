220f45dc95736d2dac106b839df92b2ac0eebb9f
farzeencode <145785539+farzeencode@users.noreply.github.com>
2023-10-01 16:51:20 +0530

    Create far.c

---
+++ 4.C/far.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+ int main()
+  {
+     printf("Hello!\n");
+     printf("Name   : far\n");
+     printf("Age    : 19\n");
+     printf("Username : far-YT\n");
+     return(0);
+  }
