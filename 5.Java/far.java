fc40ad5f1f51d1958169db678a1ce757909f251c
farzeencode <145785539+farzeencode@users.noreply.github.com>
2023-10-01 16:48:27 +0530

    Create far.java

---
+++ 5.Java/far.java
@@ -0,0 +1,10 @@
+class Far {
+    public static void main(String[] args) {
+
+    String name = "far";
+    int age = 22 ;
+    String username = "far";
+
+        System.out.println("Hello!\nName: "+name+"\nAge: "+age+ "\nUsername: "+username);
+    }
+}
